adc8a7b669a1f71dce0902d341ccd03236cd2bd8
Navid Hasan <53113814+navid-hasan26@users.noreply.github.com>
2022-11-21 11:27:28 +0600

    Added Test(Except Customer)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Shop shop   =   Shop.getInstance();
-       //ustomer newCustomer = new Customer("Navid_026");
         System.out.println(shop.getAdmins());
     }
 }
---
+++ src/test/java/org/example/ShopTest.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getAdmins() {
+    }
+
+    @Test
+    void getCustomers() {
+    }
+
+    @Test
+    void getProducts() {
+    }
+
+    @Test
+    void getCarts() {
+    }
+
+    @Test
+    void addAdmin() {
+    }
+
+    @Test
+    void removeAdmin() {
+    }
+
+    @Test
+    void addCustomer() {
+    }
+
+    @Test
+    void addCarts() {
+    }
+
+    @Test
+    void removeCustomer() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void removeProduct() {
+    }
+
+    @Test
+    void makeShipments() {
+    }
+
+    @Test
+    void confirmDelivery() {
+    }
+
+    @Test
+    void modifyProduct() {
+    }
+}
